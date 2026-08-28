0a9eb6ff8d0ee2db464dba7617f918225b8c5c4a
biasfari <biasfari@gmail.com>
2026-08-27 22:19:46 -0300

    Add files via upload

---
+++ LOJA/js/nome.js
@@ -0,0 +1,12 @@
+//variável
+var nome = prompt("digite seu nome:");
+console.log("seu nome é: ", nome)
+alert("Seu nome é:  "+nome);
+var idade = prompt("digite sua idade:");
+console.log("seu nome é: ", nome)
+console.log("sua idade é: ", idade)
+alert("Sua idade é:  "+Idade);
+var cpf = prompt("digite seu cpf:");
+console.log("seu cpf é: ",cpf)
+console.log("seu cpf é: ", cpf)
+alert("Seu cpf é:  "+CPF);
